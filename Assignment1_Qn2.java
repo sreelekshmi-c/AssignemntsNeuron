08f0dd270b6c69b81b91019c9627afae6261e20f
sreelekshmi-c <60432788+sreelekshmi-c@users.noreply.github.com>
2022-10-13 14:17:37 +0530

    Add files via upload

---
+++ Assignment1_Qn2.java
@@ -0,0 +1,34 @@
+package in.ineuron.main;
+
+public class Assignment1_Qn2 {
+
+	public static void main(String[] args) {
+		
+		for (int x=0;x<4;x++)
+		{
+			for (int y=0;y<4;y++)
+			{
+				if(x==0)
+				{
+					System.out.print("1 ");
+				}
+				if(x==1)
+				{
+					System.out.print("2 ");
+				}
+				if(x==2)
+				{
+					System.out.print("3 ");
+				}
+				if(x==3)
+				{
+					System.out.print("4 ");
+				}
+			}
+			System.out.println("");
+		}
+		
+
+	}
+
+}
