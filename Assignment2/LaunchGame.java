a6044763f61605096cc2c44bb8d33943a1d90c90
sreelekshmi-c <60432788+sreelekshmi-c@users.noreply.github.com>
2022-10-29 11:00:00 +0530

    Create LaunchGame.java

---
+++ Assignment2/LaunchGame.java
@@ -0,0 +1,12 @@
+
+public class LaunchGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Umpire u=new Umpire();
+u.collectNumFromGuesser();
+u.CollectNumFromPlayer();
+u.compare();
+	}
+
+}
